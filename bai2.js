f6b4df5da67cfcb660dff17948f49fb997f79f8b
Nguyen Tien Dat <tiendat2k3.dev@gmail.com>
2026-07-29 20:08:12 +0700

    them tu 1 -6

--- bai2.js
+++
@@ -1,7 +0,0 @@
-export function home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
-}
--- bai3.js
+++
@@ -1,9 +0,0 @@
-import { bai2 } from "./bai2.js";
-const App = () => {
-  return (
-    <div>
-      <App />
-      <bai2 />
-    </div>
-  );
-};
